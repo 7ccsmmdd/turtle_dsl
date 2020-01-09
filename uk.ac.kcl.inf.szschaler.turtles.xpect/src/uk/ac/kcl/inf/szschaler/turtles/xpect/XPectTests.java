b2c56b569043569c26d87c1ab756df0304de811d
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2020-01-09 15:06:04 +0000

    Updated Xpect test to cover stats code generator

--- uk.ac.kcl.inf.szschaler.turtles.xpect/src/uk/ac/kcl/inf/szschaler/turtles/xpect/XPectTests.java
+++ uk.ac.kcl.inf.szschaler.turtles.xpect/src/uk/ac/kcl/inf/szschaler/turtles/xpect/XPectTests.java
@@ -1,10 +1,23 @@
 package uk.ac.kcl.inf.szschaler.turtles.xpect;
 
 import org.eclipse.xpect.runner.XpectRunner;
-import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+import org.eclipse.xpect.runner.XpectSuiteClasses;
+import org.eclipse.xpect.xtext.lib.tests.GeneratorTest;
+import org.eclipse.xpect.xtext.lib.tests.JvmModelInferrerTest;
+import org.eclipse.xpect.xtext.lib.tests.LinkingTest;
+import org.eclipse.xpect.xtext.lib.tests.ResourceDescriptionTest;
+import org.eclipse.xpect.xtext.lib.tests.ScopingTest;
+import org.eclipse.xpect.xtext.lib.tests.ValidationTest;
 import org.junit.runner.RunWith;
 
+@XpectSuiteClasses({ JvmModelInferrerTest.class, //
+	LinkingTest.class, //
+	ResourceDescriptionTest.class, //
+	ScopingTest.class, //
+	ValidationTest.class,  //
+})
 @RunWith(XpectRunner.class)
-public class XPectTests extends XtextTests {
+public class XPectTests extends GeneratorTest {
 	
 }
+
