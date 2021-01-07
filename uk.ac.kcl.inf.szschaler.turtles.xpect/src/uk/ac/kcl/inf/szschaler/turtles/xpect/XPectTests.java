22c482d17187605949a11b6456eed7a466693d99
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2021-01-07 12:24:22 +0000

    Update Xpect dependencies

--- uk.ac.kcl.inf.szschaler.turtles.xpect/src/uk/ac/kcl/inf/szschaler/turtles/xpect/XPectTests.java
+++ uk.ac.kcl.inf.szschaler.turtles.xpect/src/uk/ac/kcl/inf/szschaler/turtles/xpect/XPectTests.java
@@ -1,10 +1,10 @@
 package uk.ac.kcl.inf.szschaler.turtles.xpect;
 
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
 import org.junit.runner.RunWith;
-import org.xpect.runner.XpectRunner;
-import org.xpect.xtext.lib.tests.XtextTests;
 
 @RunWith(XpectRunner.class)
 public class XPectTests extends XtextTests {
-	
+
 }
